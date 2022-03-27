9683fab52e1fb9be5548f0f184cd4d8bb598b3cc
Sangwani <mightyp2@gmail.com>
2022-03-27 21:53:02 +0200

    add completed tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""generates a .tgz archive from the contents of the web_static folder """
-
+# Fabfile to generates a .tgz archive from the contents of web_static.
 import os.path
 from datetime import datetime
 from fabric.api import local
 
 
 def do_pack():
-    """ generates a .tgz archive """
+    """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
-    file = "versions/web_static_{} {} {} {} {} {}".format(dt.year,
-                                                          dt.month,
-                                                          dt.day,
-                                                          dt.hour,
-                                                          dt.month,
-                                                          dt.second)
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
